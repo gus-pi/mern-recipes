2bd3de3b2b12cc02b0e20c29f947d3470a7e61dc
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-06 13:08:32 -0400

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="max-w-screen-2xl mx-auto">
+      <nav>Navbar</nav>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
