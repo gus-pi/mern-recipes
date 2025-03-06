21a2a0db5532bc39dfdf48b48fbc875acf9cc3d7
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-06 16:16:56 -0400

    add 404 error page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './pages/home/Home.tsx';
+import ErrorPage from './components/ErrorPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
       </Route>
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   </BrowserRouter>
 );
