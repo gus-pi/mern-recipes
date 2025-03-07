362f2ef86c756b60e278a937850cfbdac5146a02
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-07 00:36:04 -0400

    add category wrapper and category page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './pages/home/Home.tsx';
 import ErrorPage from './components/ErrorPage.tsx';
+import CategoryPage from './pages/category/CategoryPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
+        <Route path="/categories/:category" element={<CategoryPage />} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
